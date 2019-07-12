57c195b8449d1e101660e6930eb7dab6fe244ef6
zcover <zerek.cover@gmail.com>
2019-07-12 14:30:30 -0700

    responding to question 3, code is not working

--- app.js
+++ app.js
@@ -46,11 +46,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  multiply.splice(3, 0, );
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+SumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
