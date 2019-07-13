f848f2c85987ebbd08ea84d14e265502b6480c90
zcover <zerek.cover@gmail.com>
2019-07-12 17:04:46 -0700

    finally figured out question 3 syntax

--- app.js
+++ app.js
@@ -39,22 +39,35 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an 
+array where the 
+  first element is the sum of those three numbers, the 
+    second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+      Third element: "4 and 7 and 5 sum to 16."
+          Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var sum1 = sum(a, b)[0];
+  var sum2 = sum(sum1, c)[0];
 
-  sum(4, 7, 5);
-  var sumanswer = ('Your answer is' + sum());
-  multiply();
-  console.log(multiply);
-  var productanswer = ('Your answer is' + multiply());
-  return [sum, multiply];
+  var sumresponse = (a + ' and ' + b + ' and ' + c + ' sum to ' + sum2 + '.');
+  // console.log(sumresponse);
 
+  // console.log(sum2);
+  // console.log('finished sum');
+
+  var pr1 = multiply(a, b)[0];
+  var pr2 = multiply(pr1, c)[0];
+
+  var prodresponse = ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + pr2 + '.');
+  // console.log(prodresponse);
+  
+  // console.log(pr2);
+  // console.log('finished product');
+  return [sum2, pr2, sumresponse, prodresponse]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
